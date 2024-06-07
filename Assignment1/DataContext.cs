438d8466b20abaa26f5f41e28f1081c50ac959a0
kapadiad2 <kapadiad2@SCE021>
2024-06-06 19:59:13 -0600

    Add project files.

---
+++ Assignment1/DataContext.cs
@@ -0,0 +1,34 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class DataContext : DbContext
+    {
+
+
+        public string DbPath { get; set; }
+
+
+        public DbSet<User> Users{ get; set; }
+        public DbSet<BlogType> BlogTypes { get; set; }
+        public DbSet<PostType> PostTypes { get; set; }
+        public DbSet<Post> Posts{ get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DataContext()
+        {
+            var path = AppContext.BaseDirectory;
+            DbPath = Path.Combine(path, "Assignment1.db");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite($"Data Source = {DbPath}");
+        }
+    }
+}
